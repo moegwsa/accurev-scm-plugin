576304953d69ffb4d159f020e0875b25de0531ee
Thomas <tmel@widex.com>
2019-11-12 13:24:22 +0100

    Fixed issues with multibranch project and top stream trait

--- src/main/java/hudson/plugins/accurev/extensions/impl/TopStreamDiscovery.java
+++ src/main/java/hudson/plugins/accurev/extensions/impl/TopStreamDiscovery.java
@@ -1,9 +1,16 @@
 package hudson.plugins.accurev.extensions.impl;
 
 import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.plugins.accurev.AccurevSCM;
 import hudson.plugins.accurev.extensions.AccurevSCMExtension;
 import hudson.plugins.accurev.extensions.AccurevSCMExtensionDescriptor;
+import jenkins.plugins.accurevclient.AccurevClient;
+import jenkins.plugins.accurevclient.commands.PopulateCommand;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
 
 public class TopStreamDiscovery extends AccurevSCMExtension {
 
@@ -23,6 +30,11 @@ public class TopStreamDiscovery extends AccurevSCMExtension {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public void decoratePopulateCommand(AccurevSCM scm, Run<?, ?> build, AccurevClient accurev, TaskListener listener, PopulateCommand cmd) throws IOException, InterruptedException {
+        super.decoratePopulateCommand(scm, build, accurev, listener, cmd);
     }
 
     @Extension
