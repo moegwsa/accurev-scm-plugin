282f9f230ddb11638aca7146d1cc807d73b2eecd
Emil EHHN. Hvid Hansen <ehhn@widex.com>
2020-02-25 10:47:35 +0100

    added functionality to checkout shared libraries

--- src/main/resources/hudson/plugins/accurev/AccurevSCM/buildEnv.groovy
+++ src/main/resources/hudson/plugins/accurev/AccurevSCM/buildEnv.groovy
@@ -2,7 +2,7 @@ package hudson.plugins.accurev.AccurevSCM
 
 def l = namespace(lib.JenkinsTagLib)
 
-['ACCUREV_TRANSACTION', 'ACCUREV_STREAM','ACCUREV_SERVER','ACCUREV_HOST'].each {name ->
+['ACCUREV_TRANSACTION', 'ACCUREV_STREAM','ACCUREV_SERVER','ACCUREV_PORT'].each {name ->
     l.buildEnvVar(name: name) {
         raw(_("${name}.blurb"))
     }
