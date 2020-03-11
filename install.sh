b3343bd38b8001570041e242d4da073e8fe1191f
Emil EHHN. Hvid Hansen <ehhn@widex.com>
2020-03-11 14:12:16 +0100

    do not overwrite exit code

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -fsSL http://cdn.microfocus.com/cached/legacymf/Products/accurev/accurev7.3/accurev-7.3-linux-x86-x64-clientonly.bin -o ./accurev-client.bin
+chmod +x ./accurev-client.bin
+./accurev-client.bin -i silent -f ./installer.properties
